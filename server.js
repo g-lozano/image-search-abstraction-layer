0d30ba2176974bedd0ac7599996cfd9ae4d4c7e4
G Lozano <gfourth1005@gmail.com>
2017-01-30 01:43:39 +0000

    update

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
-// possible apis:
-// google, flickr, yahoo
-
-// data to return:
-// url, thumbnail, summary?
-
-// insert query+date into db
+//to do:
+//api/latest/imagesearch/
 
 var mongo = require('mongodb').MongoClient
 const request = require('request')
 var express = require('express')
 var app = express()
-var http = require('http') //remove
 
 var flickrAPIKey = "api_key=<private>"
-//var flickrAPIKey = process.env.FLICKR_API_KEY
-var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&sort=relevance&" + flickrAPIKey + "&format=json&nojsoncallback=1"
+var flickrAPIKey = process.env.FLICKR_API_KEY
+var flickrURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&sort=relevance&format=json&nojsoncallback=1&per_page=10&" + flickrAPIKey
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/html/index.html')
 })
 
+//temp, for debugging
+function c(message) {
+    console.log(message)
+}
+
 app.get('/api/imagesearch/:id', function(req, res) {
     var str = req.params.id
+    var offset = Number(req.query.offset)
+
     var param = "&text=" + str
-    
-    //handle offset
-    
-    //format https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-    request(flickrURL+param, function (err, data, body) {
+    if (offset)
+        param += "&page=" + offset
+
+    request(flickrURL + param, function(err, data, body) {
         if (err) throw err
         else if (data.statusCode === 200) {
-            const json = JSON.parse(body)
-            //process data
-            res.send(json)
+            
+            var json = JSON.parse(body) //returns array
+            var photos = json.photos.photo
+            var sendData = []
+
+            photos.forEach(function(photo) {
+                var url = "https://farm"+photo.farm+".staticflickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg"
+                var page = "https://www.flickr.com/photos/"+photo.owner+"/"+photo.id
+                var temp = {
+                    title: photo.title,
+                    url: url,
+                    page : page
+                }
+                sendData.push(temp)
+            })
+
+            res.send(sendData)
         }
         else {
             console.log("error")
@@ -45,7 +59,7 @@ app.get('/api/latest/imagesearch/', function(req, res) {
     //send list of latest queries
 })
 
-app.get('*', function(req, res){
+app.get('*', function(req, res) {
     res.redirect('/')
 })
 
